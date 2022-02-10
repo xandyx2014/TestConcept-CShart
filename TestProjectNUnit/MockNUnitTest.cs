948801bb17ffb2aea76943c8466fc79c67e94873
Andy Jesus <xandyx2014@gmail.com>
2022-02-10 13:01:50 -0400

    add new topics

---
+++ TestProjectNUnit/MockNUnitTest.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using NUnit.Framework;
+
+
+namespace TestProjectNUnit
+{
+    [TestFixture]
+    internal class MockNUnitTest
+    {
+        /*private CuentaBancaria cuentaBancaria;
+        [SetUp]
+        public void Setup()
+        {
+            
+        }*/
+        [Test]
+        public void Deposito_Input_Monto_100()
+        {
+            var mocking = new Mock<ILoggerGeneral>();
+            // mocking.SetupAllProperties();
+            mocking.Setup(s => s.IsLog).Returns(true);
+            // verifica cuantas veces el mock esta lmado al metodo
+
+            // mocking.Verify(u => u.LogDatabase(It.IsAny<string>()), Times.Exactly(1));
+            var cuentaBancaria = new CuentaBancaria(mocking.Object);
+
+            var resultado = cuentaBancaria.Deposito(100);
+
+            Assert.That(resultado, Is.True);
+            Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(100));
+        }
+        [TestCase(200, 100)]
+        [TestCase(200, 150)]
+        public void Retiro_Retiro100Balance200_Returns(int balance, int retiro)
+        {
+            var mocking = new Mock<ILoggerGeneral>();
+            mocking.Setup(s => s.IsLog).Returns(true);
+            mocking.Setup(u => u.LogDatabase(It.IsAny<string>())).Returns(true);
+            // It.IsInRange<int>(int.MinValue, -1, Moq.Range.Inclusive);
+            mocking.Setup(u => u.LogBalanceDespuesRetiro(It.Is<int>(x => x > 0))).Returns(true);
+            var cuentaBancaria = new CuentaBancaria(mocking.Object);
+
+            cuentaBancaria.Deposito(balance);
+            var resultado = cuentaBancaria.Retiro(retiro);
+            Assert.IsTrue(resultado);
+        }
+        [Test]
+        public void CuentaBancariaLoggerGeneral_LogMockingOuPutReturnTrue()
+        {
+            var mocking = new Mock<ILoggerGeneral>();
+            string testText = "Hola";
+            mocking.Setup((e) => e.MessageWithOutParamsReturnBool(It.IsAny<string>(), out testText)).Returns(true);
+            string paramsOut = "";
+            var result = mocking.Object.MessageWithOutParamsReturnBool("Andy", out paramsOut);
+            Assert.IsTrue(result);
+        }
+    }
+}
--- TestProjectNUnit/OperacionLibrary.cs
+++ TestProjectNUnit/OperacionLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,4 +46,110 @@ namespace TestProjectNUnit
     { }
     internal class PremiunCliente : Cliente
     { }
+    internal class CuentaBancaria
+    {
+        private readonly ILoggerGeneral _loggerGeneral;
+
+        public int balance { get; set; }
+
+        public CuentaBancaria(ILoggerGeneral loggerGeneral)
+        {
+            balance = 0;
+            _loggerGeneral = loggerGeneral;
+        }
+        public bool Deposito(int monto)
+        {
+            _loggerGeneral.Message($"Monto de {monto}");
+            balance = balance + monto;
+            return true;
+        }
+        public bool Retiro(int monto)
+        {
+            if (monto <= balance)
+            {
+                _loggerGeneral.LogBalanceDespuesRetiro(monto);
+                balance = balance - monto;
+                return _loggerGeneral.LogBalanceDespuesRetiro(balance);
+            }
+            return _loggerGeneral.LogBalanceDespuesRetiro(balance - monto);
+        }
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+    }
+    public interface ILoggerGeneral
+    {
+        bool IsLog { get; set; }
+
+        void Message(string message);
+        bool LogDatabase(string message);
+        bool LogBalanceDespuesRetiro(int balanceDespuesRetiro);
+        bool MessageWithOutParamsReturnBool(string message, out string ouputStr);
+    }
+    class LoggerGeneral : ILoggerGeneral
+    {
+        public bool IsLog { get; set; }
+
+        public bool LogBalanceDespuesRetiro(int balanceDespuesRetiro)
+        {
+            if (balanceDespuesRetiro >= 0)
+            {
+                Console.WriteLine("Success");
+                return true;
+            }
+            return false;
+        }
+
+        public bool LogDatabase(string message)
+        {
+            Console.WriteLine(message);
+            return true;
+        }
+
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public bool MessageWithOutParamsReturnBool(string message, out string ouputStr)
+        {
+            ouputStr = "Hola" + message;
+            return true;
+        }
+    }
+    class LoggerGeneralMock : ILoggerGeneral
+    {
+        public bool IsLog { get; set; }
+
+        public bool LogBalanceDespuesRetiro(int balanceDespuesRetiro)
+        {
+            if (balanceDespuesRetiro >= 0)
+            {
+                Console.WriteLine("Success");
+                return true;
+            }
+            return false;
+        }
+
+        public bool LogDatabase(string message)
+        {
+            Console.WriteLine(message);
+            return true;
+        }
+
+
+        public void Message(string message)
+        {
+
+        }
+
+        public bool MessageWithOutParamsReturnBool(string message, out string ouputStr)
+        {
+            ouputStr = "Hola" + message;
+            return true;
+        }
+    }
+
 }
